22eff0e46e41d955a6a61d6201372f9d457ae5b1
Amir mishori <amirbrb@gmail.com>
2018-12-24 20:39:40 +0200

    clean up

--- src/components/general/App.vue
+++ src/components/general/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="app">
-    <HeaderNavigation></HeaderNavigation>
     <div class="route-content">
-      asaaaa
     </div>
   </div>
 </template>
 
 <script>
-  import HeaderNavigation from './HeaderNavigation.vue'
   export default {
     name: 'app',
-    components: {HeaderNavigation},
+    components: {},
     methods: {
     },
     data: function () {
@@ -21,16 +18,8 @@
 </script>
 
 <style>
-  @import '../../../static/css/site.css';
+  @import '/static/css/site.css';
 
   .app{
-  }
-
-  .route-content{
-    background: gray;
-    margin-top: 70px;
-    padding: 10px;
-    height: 150vh;
-    overflow: auto;
   }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,5 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
 	state: {
-		userData: {
-			profileImage: ''
-		}
 	}
 })
